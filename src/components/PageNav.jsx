0a4af556c47a5b04edc1b6997ac42e971c5e7f10
muzamilsattar <muzixtra@gmail.com>
2024-11-23 13:45:44 +0500

    Add project files

---
+++ src/components/PageNav.jsx
@@ -0,0 +1,28 @@
+import { Link, NavLink } from "react-router-dom";
+import styles from "./PageNav.module.css";
+import Logo from "./Logo";
+function PageNav() {
+  return (
+    <nav className={styles.nav}>
+      <Link to='/'>
+        <Logo />
+      </Link>
+      <ul>
+        <li>
+          <NavLink to='/pricing'>Pricing</NavLink>
+        </li>
+
+        <li>
+          <NavLink to='/product'>Product</NavLink>
+        </li>
+        <li>
+          <NavLink className={styles.ctaLink} to='/login'>
+            Login
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default PageNav;
